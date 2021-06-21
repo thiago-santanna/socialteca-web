4407146d22ca4865ebedf4344a19cab501533037
xthiagosant <uzmaki_thiago@outlook.com>
2021-06-21 14:38:46 -0300

    alteração no config do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        red: {
-          600: '#E02041' //Destaque
-        },
-        gray: {
-          300: '#A8A8B3', //Textos secundários
-          400: '#737380', // Textos
-          700: '#41414D', // Títulos secundários
-          900: '#13131A', // Títulos primários
-        },
-      },      
-    },
+    extend: {},
   },
   variants: {
     extend: {},
